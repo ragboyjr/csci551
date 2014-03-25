cdf19f09cb57d942711cb61b9d303b61d20b8e5d
RJ garcia <rj@bighead.net>
2014-03-25 01:21:41 -0700

    building the run script
    Signed-off-by: RJ garcia <rj@bighead.net>

--- asst2/run-tests.py
+++ asst2/run-tests.py
@@ -1,36 +1,116 @@
 import re
 import sys
+import os
+import pprint
+from subprocess import Popen, PIPE
 
-test_str = """Enter a, b, and n
-100 
-600 
-1000000 
+class MPIRun:
 
-Running on 10 processors.
-Elapsed time = 5.884409e-02 seconds 
-With n = 1000000 trapezoids, 
-our estimate of the integral from 100.000000 to 600.000000 = 4.0037209001535e+03 
-absolute relative true error = 5.539354e-13 is NOT less than criteria = 5.000000e-13"""
+    MAX_RUNS  = 5
+    HOST_FILE = "../machine-assignments.txt"
+    NUM_SLOTS = 2
 
-float_e_pattern  = "(\d+.\d+e(-|\+)?\d+)"
-anything_pattern = "[.\s]+"
+    def __init__(self, a, b, n, p):
+        self.a = a
+        self.b = b
+        self.n = n
+        self.p = p
+        self.results = []
+        
+        # create the re pattern
+        self._create_pattern()
+        self._get_hosts()
+    
+    def run(self):
+        
+        i = 0
+        while i < MPIRun.MAX_RUNS:
+            self.results.append(self._mpirun())
+            i++
+        
+        # sort the results
+        self.results = sorted(self.results, cmp=lambda a, b: compare(a.elapsed_time, b.elapsed_time))
+        
+    def _mpirun(self):
+        hostfile = self._create_host_file()
+        
+        inp_args = ['echo', '"%d %d %d"' % (self.a, self.b, self.n)]
+        mpi_args = ['mpirun', '--prefix', '/opt/openmpi', '-hostfile', hostfile, "./bin/integrate"]
+        
+        p1 = Popen(inp_args, stdout=PIPE)
+        p2 = Popen(mpi_args, stdin=p1.stdout, stdout=PIPE)
+        p1.stdout.close()
+        output = p2.communicate()[0]
+        
+        return self._parse_output(output)
+        
+    
+    def _parse_output(self, output):
+        m = self.re.search(output)
+        data = {
+            "num_proccessors" : int(m.group(1)),
+            "elapsed_time"    : float(m.group(2)),
+            "num_trapezoids"  : int(m.group(4)),
+            "a"               : float(m.group(5)),
+            "b"               : float(m.group(6)),
+            "result"          : float(m.group(7)),
+            "abs_rel_t_error" : float(m.group(9)),
+            "min_error"       : float(m.group(11)),
+        }
+        return data
+        
+    
+    def _create_pattern(self):
+        float_e_pattern     = "(\d+.\d+e(-|\+)?\d+)"
+        end_of_line_pattern = ".+\s+"
 
-output_re_pattern = \
-    r"Running on (\d+) processors%(any)s" + \
-    r"Elapsed time = %(float)s seconds\n(With) %(any)s"# + \
-#    r"With"
-#    r"With n = (\d+)%(any)s" + \
-#    r"our estimate.+from (\d+\.\d+) to (\d+\.\d+) = %(float)s"
+        output_re_pattern = \
+            r"Running on (\d+) processors%(eol)s" + \
+            r"Elapsed time = %(float)s %(eol)s" + \
+            r"With n = (\d+)%(eol)s" + \
+            r"our estimate.+from (\d+\.\d+) to (\d+\.\d+) = %(float)s%(eol)s" + \
+            r"absolute .+ = %(float)s .+ %(float)s"
 
-output_re_pattern = output_re_pattern % \
-                    {"float": float_e_pattern, "any": anything_pattern}
+        output_re_pattern = output_re_pattern % \
+                            {"float": float_e_pattern, "eol": end_of_line_pattern}
 
-print output_re_pattern
+        self.re = re.compile(output_re_pattern, re.M);
+    
+    def _get_hosts(self):        
+        f = open(MPIRun.HOST_FILE, "r")
+                
+        for line in f:
+            self.hosts.append(line.strip())
+            
+        self.hosts.sort()
+        
+        f.close()
+        
+    def _create_host_file(self):
+        
+        # the hosts file depends on the number of processes
+        file_name = "%d-hosts.txt" % (self.p)
+        
+        if os.path.exists(file_name):
+            return file_name
+        
+        # create the file
+        f = open(file_name, "w")
+        
+        num_hosts = int(self.p / MPIRun.NUM_SLOTS) # two processes per host
+        
+        i = 0
+        while i < num_hosts:
+            host = self.hosts[i]
+            f.write("%s slots=%d\n" % (host, MPIRun.NUM_SLOTS))
+            i++
+        f.close()
+        
+        return file_name
 
-output_re = re.compile(output_re_pattern, re.M);
-m = output_re.search(test_str)
+pp = pprint.PrettyPrinter(indent=4)
 
-if m:
-    print m.groups()
-else:
-    print "No matches found"
+run = MPIRun(100, 600, 10000, 2)
+run.run()
+
+pp.print(run.results)
